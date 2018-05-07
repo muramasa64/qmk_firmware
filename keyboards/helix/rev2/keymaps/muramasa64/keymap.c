6cc215de9a4e4d13125c1a3068738e275448ae0f
ISOBE Kazuhiko <muramasa64@gmail.com>
2018-05-08 00:33:51 +0900

    update

--- keyboards/helix/rev2/keymaps/muramasa64/keymap.c
+++ keyboards/helix/rev2/keymaps/muramasa64/keymap.c
@@ -67,17 +67,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | EISU | KANA |   N  |   M  |   ,  |   .  |   /  | `    |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |      |   N  |   M  |   ,  |   .  |   /  | `    |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust|Lower |      | Alt  | GUI  | HENK |Space |Space | MHEN | GUI  |  Alt |      |Raise |Enter |
+   * |Adjust|      | Alt  | GUI  | EISU |Space |Lower |Raise |Space | KANA | GUI  |  Alt |      |Enter |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
-      KC_ESC,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-      KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
-      KC_LCTL, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    EISU,   KANA,   KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV , \
-      ADJUST,  LOWER, XXXXXXX, KC_LALT, KC_LGUI, KC_HENK, KC_SPC, KC_SPC, KC_MHEN, KC_RGUI, KC_RALT  XXXXXXX, RAISE,   KC_ENT \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4, KC_5,                     KC_6,   KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R, KC_T,                     KC_Y,   KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F, KC_G,                     KC_H,   KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V, KC_B,   XXXXXXX, XXXXXXX, KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_GRV , \
+      ADJUST,  XXXXXXX, KC_LALT, KC_LGUI, EISU, KC_SPC, LOWER,   RAISE,   KC_SPC, KANA, KC_RGUI, KC_RALT, XXXXXXX, KC_ENT \
       ),
 
   /* Dvorak
@@ -88,17 +88,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   O  |   E  |   U  |   I  |             |   D  |   H  |   T  |   N  |   S  |  -   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   ;  |   Q  |   J  |   K  |   X  | EISU | KANA |   B  |   M  |   W  |   V  |   Z  | `    |
+   * | Shift|   ;  |   Q  |   J  |   K  |   X  |      |      |   B  |   M  |   W  |   V  |   Z  | `    |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust|Lower |      | Alt  | GUI  | HENK |Space |Space | MHEN | GUI  | Alt  |      |Raise |Enter |
+   * |Adjust|      | Alt  | GUI  | EISU |Space |Lower |Raise |Space | KANA | GUI  |  Alt |      |Enter |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_DVORAK] = KEYMAP( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-      KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                    KC_F,  KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
-      KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                    KC_D,  KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
-      KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    EISU,   KANA,   KC_B,  KC_M,    KC_W,    KC_V,    KC_Z,    KC_GRV , \
-      ADJUST,  LOWER,   XXXXXXX, KC_LALT, KC_LGUI, KC_HENK, KC_SPC, KC_SPC, KC_MHEN, KC_RGUI, KC_RALT  XXXXXXX, RAISE,   KC_ENT \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4, KC_5,                     KC_6,   KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P, KC_Y,                     KC_F,   KC_G, KC_C,    KC_R,    KC_L,    KC_SLSH, \
+      KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U, KC_I,                     KC_D,   KC_H, KC_T,    KC_N,    KC_S,    KC_MINS, \
+      KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K, KC_X,   XXXXXXX, XXXXXXX, KC_B,   KC_M, KC_W,    KC_V,    KC_Z,    KC_GRV , \
+      ADJUST,  XXXXXXX, KC_LALT, KC_LGUI, EISU, KC_SPC, LOWER,   RAISE,   KC_SPC, KANA, KC_RGUI, KC_RALT, XXXXXXX, KC_ENT \
       ),
 
   /* Lower
