c5d3e9d84fcc16aa438b8082ca1cb492dead1845
ISOBE Kazuhiko <muramasa64@gmail.com>
2018-09-03 15:55:44 +0900

    Change some modifiers
    * Mod-tap Right Ctrl, Right Shift
    * Cut, Copy, Paste shortcut key

--- keyboards/helix/rev2/keymaps/muramasa64/keymap.c
+++ keyboards/helix/rev2/keymaps/muramasa64/keymap.c
@@ -67,19 +67,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |   [  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |CTL(')|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |      |   N  |   M  |   ,  |   .  |   /  | `    |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |      |   N  |   M  |   ,  |   .  |   /  |SFT(`)|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |KeyPad|Adjust| Alt  | GUI  | EISU |Lower |Raise |Space |Raise | KANA | GUI  | Alt  |      |Enter |
+   * |KeyPad|Adjust| Alt  | GUI  | EISU | Lower| Raise| Space| Raise| KANA | GUI  | Alt  | Lower|Enter |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
       KC_ESC,  KC_1,   KC_2,    KC_3,    KC_4, KC_5,                          KC_6,  KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R, KC_T,                          KC_Y,  KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, \
-      KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F, KC_G,                          KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V, KC_B,  XXXXXXX, XXXXXXX,       KC_N,  KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_GRV , \
-      KEYPAD,  ADJUST, KC_LALT, KC_LGUI, EISU, LOWER, RAISE,   SFT_T(KC_SPC), RAISE, KANA, KC_RGUI, KC_RALT, XXXXXXX, KC_ENT \
+      KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F, KC_G,                          KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT), \
+      KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V, KC_B,  XXXXXXX, XXXXXXX,       KC_N,  KC_M, KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_GRV) , \
+      KEYPAD,  ADJUST, KC_LALT, KC_LGUI, EISU, LOWER, RAISE,   SFT_T(KC_SPC), RAISE, KANA, KC_RGUI, KC_RALT, LOWER,   KC_ENT \
       ),
 
   /* Dvorak
@@ -88,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   '  |   ,  |   .  |   P  |   Y  |             |   F  |   G  |   C  |   R  |   L  |  /   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |   A  |   O  |   E  |   U  |   I  |             |   D  |   H  |   T  |   N  |   S  |  -   |
+   * | Ctrl |   A  |   O  |   E  |   U  |   I  |             |   D  |   H  |   T  |   N  |   S  |-(CTL)|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   ;  |   Q  |   J  |   K  |   X  |      |      |   B  |   M  |   W  |   V  |   Z  | `    |
+   * | Shift|   ;  |   Q  |   J  |   K  |   X  |      |      |   B  |   M  |   W  |   V  |   Z  |`(SFT)|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |KEYPAD|Adjust| Alt  | GUI  | EISU |Lower |Raise |Space |Raise | KANA | GUI  | Alt  |Lower |Enter |
    * `-------------------------------------------------------------------------------------------------'
@@ -98,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK] = LAYOUT( \
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4, KC_5,                          KC_6,  KC_7, KC_8,    KC_9,    KC_0,  KC_BSPC, \
       KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P, KC_Y,                          KC_F,  KC_G, KC_C,    KC_R,    KC_L,  KC_SLSH, \
-      KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U, KC_I,                          KC_D,  KC_H, KC_T,    KC_N,    KC_S,  KC_MINS, \
-      KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K, KC_X,  XXXXXXX, XXXXXXX,       KC_B,  KC_M, KC_W,    KC_V,    KC_Z,  KC_GRV , \
+      KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U, KC_I,                          KC_D,  KC_H, KC_T,    KC_N,    KC_S,  RCTL_T(KC_MINS), \
+      KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K, KC_X,  XXXXXXX, XXXXXXX,       KC_B,  KC_M, KC_W,    KC_V,    KC_Z,  RSFT_T(KC_GRV) , \
       KEYPAD,  ADJUST,  KC_LALT, KC_LGUI, EISU, LOWER, RAISE,   SFT_T(KC_SPC), RAISE, KANA, KC_RGUI, KC_RALT, LOWER, KC_ENT \
       ),
 
@@ -142,9 +142,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  | F10  | F11  | F12  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             |      | Home |  Up  | End  |      |      |
+   * |      |      |      | Copy |      |      |             |      | Home |  Up  | End  |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             |      | Left | Down |Right |      |      |
+   * |      |      |      | Cut  | Paste|      |             |      | Left | Down |Right |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      |PageUp|PageDn|      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -152,11 +152,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-      _______, _______, _______, _______, _______, _______,                   _______, KC_HOME, KC_UP,   KC_END,  _______, _______, \
-      _______, _______, _______, _______, _______, _______,                   _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,      KC_F5,      KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+      _______, _______, _______, LGUI(KC_I), _______,    _______,                   _______, KC_HOME, KC_UP,   KC_END,  _______, _______, \
+      _______, _______, _______, LGUI(KC_B), LGUI(KC_DOT), _______,                   _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+      _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______, _______, \
+      _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
 
   /* KeyPad (Drovak)
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |   -  |   1  |   2  |   3  |   A  |   B  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |   0  |   ,  |   .  |      |      |
+   * |KeyPad|      |      |      |      |      |      |      |      |   0  |   ,  |   .  |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_KEYPAD] = LAYOUT( \
