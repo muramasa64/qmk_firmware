fd66e206908c70233b5a77f2b977ebe509b2e788
ISOBE Kazuhiko <muramasa64@gmail.com>
2018-09-10 16:27:46 +0900

    Add lang8, lang9 key on bottom line for nicola

--- keyboards/helix/rev2/keymaps/five_rows_muramasa64/keymap.c
+++ keyboards/helix/rev2/keymaps/five_rows_muramasa64/keymap.c
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, \
       KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, RCTL_T(KC_QUOT), \
       KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  EISU, KANA, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_GRV), \
-      MO(_KEYPAD), MO(_ADJUST), KC_LALT, KC_LGUI, EISU, MO(_LOWER), MO(_RAISE), \
-                                                   SFT_T(KC_SPC), LT(_RAISE,KC_ENT), KANA, KC_RGUI, KC_RALT, MO(_LOWER), KC_ENT \
+      MO(_KEYPAD), MO(_ADJUST), KC_LALT, KC_LGUI, KC_LANG8, MO(_LOWER), MO(_RAISE), \
+                                                   SFT_T(KC_SPC), LT(_RAISE,KC_ENT), KC_LANG9, KC_RGUI, KC_RALT, MO(_LOWER), KC_ENT \
       ),
 
   /* Dvorak
@@ -96,8 +96,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,              KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH, \
       KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,              KC_D,    KC_H,    KC_T,    KC_N,    KC_S, RCTL_T(KC_MINS), \
       KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X, EISU, KANA,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, RSFT_T(KC_GRV), \
-      MO(_KEYPAD), MO(_ADJUST), KC_LALT, KC_LGUI, EISU, MO(_LOWER), MO(_RAISE), \
-                                                   SFT_T(KC_SPC), LT(_RAISE,KC_ENT), KANA, KC_RGUI, KC_RALT, MO(_LOWER), KC_ENT \
+      MO(_KEYPAD), MO(_ADJUST), KC_LALT, KC_LGUI, KC_LANG8, MO(_LOWER), MO(_RAISE), \
+                                                   SFT_T(KC_SPC), LT(_RAISE,KC_ENT), KC_LANG9, KC_RGUI, KC_RALT, MO(_LOWER), KC_ENT \
       ),
 
   /* KeyPad (Drovak)
