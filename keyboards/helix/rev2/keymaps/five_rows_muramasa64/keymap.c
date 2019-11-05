0fcca7dd1b81ba9b9acd6cf99b5f803863cabc37
ISOBE Kazuhiko <muramasa64@gmail.com>
2019-11-05 21:02:13 +0900

    change: Right KeyPad to Space (SandS)

--- keyboards/helix/rev2/keymaps/five_rows_muramasa64/keymap.c
+++ keyboards/helix/rev2/keymaps/five_rows_muramasa64/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   ;  |   Q  |   J  |   K  |   X  | EISU | KANA |   B  |   M  |   W  |   V  |   Z  |  `   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |KeyPad|Adjust| Alt  | GUI  | LANG8| Lower|KeyPad| Space| Raise| LANG9| GUI  | Alt  |GuiSft|Enter |
+   * |KeyPad|Adjust| Alt  | GUI  | LANG8| Lower| Space| Space| Raise| LANG9| GUI  | Alt  |GuiSft|Enter |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_DVORAK] = LAYOUT( \
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_QUOT, KC_COMM, KC_DOT, KC_P,    KC_Y,             KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, \
       KC_LCTL, KC_A,    KC_O,    KC_E,   KC_U,    KC_I,             KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, \
       KC_LSFT, KC_SCLN, KC_Q,    KC_J,   KC_K,    KC_X, EISU, KANA, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_GRV, \
-      MO(_KEYPAD), MO(_ADJUST), KC_LALT, KC_LGUI, KC_LANG8, LT(_LOWER,KC_BSPC), MO(_KEYPAD), \
+      MO(_KEYPAD), MO(_ADJUST), KC_LALT, KC_LGUI, KC_LANG8, LT(_LOWER,KC_BSPC), SFT_T(KC_SPC), \
                                                    SFT_T(KC_SPC), LT(_RAISE,KC_ENT), KC_LANG9, KC_RGUI, KC_RALT, RGUI(KC_RSFT), KC_ENT \
       ),
 
