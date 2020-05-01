8f9e93012a28c85e4ff1fdc5ffb27025c2248a9a
Isobe Kazuhiko <muramasa64@gmail.com>
2020-05-01 21:08:05 +0900

    update comment

--- keyboards/helix/rev2/keymaps/naginata/keymap.c
+++ keyboards/helix/rev2/keymaps/naginata/keymap.c
@@ -263,14 +263,12 @@ static int current_default_layer;
 
 uint32_t default_layer_state_set_kb(uint32_t state) {
     // 1<<_QWERTY  - 1 == 1 - 1 == _QWERTY (=0)
-    // 1<<_COLEMAK - 1 == 2 - 1 == _COLEMAK (=1)
+    // 1<<_DVORAK - 1 == 2 - 1 == _DVORAK (=1)
     current_default_layer = state - 1;
-    // 1<<_DVORAK  - 2 == 4 - 2 == _DVORAK (=2)
+    // 1<<_NAGINATA  - 2 == 4 - 2 == _NAGINATA (=2)
     if ( current_default_layer == 3 ) current_default_layer -= 1;
-    // 1<<_EUCALYN - 5 == 8 - 5 == _EUCALYN (=3)
+    // 1<<_KEYPAD - 5 == 8 - 5 == _KEYPAD (=3)
     if ( current_default_layer == 7 ) current_default_layer -= 4;
-    // 1<<_KEYPAD  - 12 == 16 - 12 == _KEYPAD (=4)
-    if ( current_default_layer == 15 ) current_default_layer -= 11;
     return state;
 }
 
